f07dfc57fb4d2483fedca68a01cf09cb9240a8ff
Paula Pniewska <pniewska.paula@gmail.com>
2025-01-08 11:05:44 +0100

    add basic front-end app components

---
+++ client/src/components/Logo/Logo.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Tilt from "react-parallax-tilt";
+import brain from "./icons8-brain-connections-96.png";
+import "./Logo.css";
+
+const Logo = () => {
+  return (
+    <div className="ma4 mt0">
+      <Tilt
+        className="Tilt br2 shadow-2"
+        tiltMaxAngleX="50"
+        tiltMaxAngleY="50"
+        style={{ height: 150, width: 150, borderRadius: 80 }}
+      >
+        <img src={brain} alt="brain" style={{ paddingTop: "30px" }} />
+      </Tilt>
+    </div>
+  );
+};
+
+export default Logo;
