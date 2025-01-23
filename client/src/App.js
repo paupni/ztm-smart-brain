6eb8baf75e2933b2e61bafff1a0edf019fbc3b3c
Paula Pniewska <pniewska.paula@gmail.com>
2025-01-23 19:08:12 +0100

    remove clarifai api from the project

--- client/src/App.js
+++ client/src/App.js
@@ -9,48 +9,6 @@ import { Component } from "react";
 import SignIn from "./components/SignIn/SignIn";
 import Register from "./components/Register/Register";
 
-const returnClarifaiRequestOptions = (imageUrl) => {
-  // Your PAT (Personal Access Token) can be found in the Account's Security section
-  const PAT = "49d1a16b8fd6444eb33db2955e1cd120";
-  // Specify the correct user_id/app_id pairings
-  // Since you're making inferences outside your app's scope
-  const USER_ID = "54dku24zoqpax";
-  const APP_ID = "ztm-smart-brain";
-  // Change these to whatever model and image URL you want to use
-  const MODEL_ID = "face-detection";
-  // const MODEL_VERSION_ID = "6dc7e46bc9124c5c8824be4822abe105";
-  // const IMAGE_URL = "https://samples.clarifai.com/metro-north.jpg";
-  const IMAGE_URL = imageUrl;
-
-  const raw = JSON.stringify({
-    user_app_id: {
-      user_id: USER_ID,
-      app_id: APP_ID,
-    },
-    inputs: [
-      {
-        data: {
-          image: {
-            url: IMAGE_URL,
-          },
-        },
-      },
-    ],
-  });
-
-  const requestOptions = {
-    // mode: "no-cors",
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      Authorization: "Key " + PAT,
-    },
-    body: raw,
-  };
-
-  return requestOptions;
-};
-
 class App extends Component {
   constructor() {
     super();
@@ -82,40 +40,33 @@ class App extends Component {
     });
   };
 
-  // componentDidMount() {
-  //   fetch("http://localhost:3000")
-  //     .then((response) => response.json())
-  //     .then(console.log);
-  // }
-
   onInputChange = (event) => {
     this.setState({ input: event.target.value });
   };
 
   onButtonSubmit = () => {
-    console.log("click");
     this.setState({ imageUrl: this.state.input });
-    // app.models.predict("face-detection", this.state.input);
+    console.log("click");
     fetch(
-      // "https://api.clarifai.com/v2/models/" +
-      "https://api.clarifai.com/main/models/" +
-        "face-detection" + //MODEL_ID
-        // "/versions/" +
-        // MODEL_VERSION_ID +
-        "/outputs",
-      returnClarifaiRequestOptions(this.state.input)
+      "http://localhost:3000/image",
+      {
+        method: "put",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ id: this.state.user.id }),
+      },
+      { mode: "no-cors" }
     )
       .then((response) => response.json())
-      .then((response) => {
-        console.log("hi", response);
-        if (response) {
-          fetch("http://localhost:3000/image", {
-            method: "put",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ id: this.state.user.id }),
-          });
-        }
-      });
+      .then((count) => {
+        console.log("hi");
+
+        this.setState({
+          user: {
+            entries: count,
+          },
+        });
+      })
+      .catch((error) => console.log("error", error));
   };
 
   onRouteChange = (route) => {
@@ -139,7 +90,10 @@ class App extends Component {
         {this.state.route === "home" ? (
           <div>
             <Logo />
-            <Rank />
+            <Rank
+              name={this.state.user.name}
+              entries={this.state.user.entries}
+            />
             <ImageLinkForm
               onInputChange={this.onInputChange}
               onButtonSubmit={this.onButtonSubmit}
@@ -147,7 +101,7 @@ class App extends Component {
             <FaceRecognition imageUrl={this.state.imageUrl} />
           </div>
         ) : this.state.route === "signin" ? (
-          <SignIn onRouteChange={this.onRouteChange} />
+          <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
         ) : (
           <Register
             loadUser={this.loadUser}
--- server/server.js
+++ server/server.js
@@ -47,7 +47,7 @@ app.post("/signin", (req, res) => {
     req.body.email === database.users[0].email &&
     req.body.password === database.users[0].password
   ) {
-    res.json("success");
+    res.json(database.users[0]);
   } else {
     res.status(400).json("error logging in");
   }
@@ -87,7 +87,7 @@ app.get("/profile/:id", (req, res) => {
   }
 });
 
-app.post("/image", (req, res) => {
+app.put("/image", (req, res) => {
   const { id } = req.body;
   let found = false;
   database.users.forEach((user) => {
